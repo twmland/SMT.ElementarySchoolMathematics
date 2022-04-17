2f2f40ae073f3ca65c09780246c0ab8491887b5f
King Liu <twmland@gmail.com>
2022-04-18 02:07:58 +0800

    create a simple web site

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('Test')
+var express =require('express');
+var app=express();
+
+app.get('/test',function(req,res){
+   console.log(req.url);
+   res.send("response from /test");
+
+})
+
+
+var port=3000;
+app.listen(port);
