63c7c37467f640cb621d29f30818f2f4ce3b3abb
King Liu <twmland@gmail.com>
2022-04-18 02:14:53 +0800

    app.js build a web

--- app.js
+++ app.js
@@ -3,10 +3,20 @@ var app=express();
 
 app.get('/test',function(req,res){
    console.log(req.url);
-   res.send("response from /test");
+   res.send("response from '/test'");
 
 })
 
 
 var port=3000;
-app.listen(port);
+app.listen(port,function(err){
+    var line='------------------------------------------';
+    console.log(line);
+    if(err){
+        console.log(err);
+    }
+    else{
+        console.log("node.js Website is listening; Port:" + port);
+    }
+    console.log(line);
+} );
